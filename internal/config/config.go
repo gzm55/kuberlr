d1a044555160a51aa2bb67e8bcdcd20de3e91b0f
James Z.M. Gao <gaozm55@gmail.com>
2024-03-19 20:03:49 +0800

    fix typo

--- internal/config/config.go
+++ internal/config/config.go
@@ -61,7 +61,7 @@ func (c *Cfg) Load() (*viper.Viper, error) {
 }
 
 func mergeConfig(v *viper.Viper, extraConfigPath string) error { //nolint: varnamelen
-	if len(s) == 0 {
+	if len(extraConfigPath) == 0 {
 		return nil
 	}
 	cfgFile := filepath.Join(extraConfigPath, "kuberlr.conf")
